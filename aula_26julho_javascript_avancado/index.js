f5f498a7b130678dbe176b9f1a66d1585fa0bf84
instrutorvilsonmoro@gmail.com <instrutorvilsonmoro@gmail.com>
2024-07-26 19:40:08 -0300

    update git

--- aula_26julho_javascript_avancado/index.js
+++ aula_26julho_javascript_avancado/index.js
@@ -1,4 +1,25 @@
 // npm install express
 // npm install
+// ejs, pug
+// npm install i ejs
 
 var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.send('<h1>Bem-vindo!</h1>');
+})
+
+app.get('/home', function(req, res) {
+    res.render('../views/home');
+})
+
+app.get('/contato', function(req, res) {
+    res.render('../views/home');
+})
+
+app.listen(3000, function(){
+    console.log('Executando em [localhost:3000] \nCtrl+C para abortar o NODE JS.')
+});
