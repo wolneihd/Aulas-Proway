6edefca79fc9d98b40c92c9f20223e04ec3de0ee
instrutorvilsonmoro@gmail.com <instrutorvilsonmoro@gmail.com>
2024-08-05 21:57:11 -0300

    update

--- modulo_Typescript/aula_31julho/basics/tipoDado.js
+++ modulo_Typescript/aula_31julho/basics/tipoDado.js
@@ -1,10 +1,12 @@
 var nomes = ["Bob"];
 nomes.push("Patrick");
-nomes.push("Antony");
-nomes.pop();
+nomes.push("Antony", "Jack");
+nomes.pop(); // remove o último elemento
+nomes.shift(); // remove o primeiro elemento 
+nomes.unshift("Harry", "Jim"); // insere no inicio do array
 // imprimir dados
 for (var i = 0; i < nomes.length; i++) {
-    console.log(nomes[i]);
+    console.log("".concat(i, " ").concat(nomes[i]));
 }
 var dados = ["Wolnei", 32, false];
 // imprimir dados
@@ -16,3 +18,43 @@ var elemento = [55, "Robert", false];
 for (var i = 0; i < elemento.length; i++) {
     console.log("".concat(i, " ").concat(elemento[i], " ").concat(typeof (elemento[i])));
 }
+// objeto
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function () {
+        return "Name: ".concat(this.firstName, " ").concat(this.lastName);
+    }
+};
+console.log(person.fullName());
+// Exemplo FOR
+var harryPotter = ["Harry", "Hermione", "Rony", "Draco", "Luna", "Hermione"];
+var posicaoArray = [];
+for (i = 0; i < harryPotter.length; i++) {
+    if (harryPotter[i] === "Hermione") {
+        posicaoArray.push(i);
+    }
+}
+console.log("Hermione[for]: ".concat(posicaoArray));
+// Exemplo FOR-EACH
+var posicaoArray_2 = [];
+harryPotter.forEach(function (nome, pos) {
+    if (nome === "Hermione") {
+        posicaoArray_2.push(pos);
+    }
+});
+console.log("Hermione[forEach]: ".concat(posicaoArray_2));
+// outro exemplo forEach
+function exibirNome(nome, pos) {
+    console.log("- ".concat(nome, " esta na posicao ").concat(pos));
+}
+harryPotter.forEach(exibirNome);
+// Quantidade de Hermione:
+var quantidadeHermione = 0;
+harryPotter.forEach(function (nome) {
+    if (nome === "Hermione") {
+        quantidadeHermione++;
+    }
+});
+console.log("Quantidade nomes Hermione: ".concat(quantidadeHermione, "."));
--- modulo_Typescript/aula_31julho/basics/tipoDado.ts
+++ modulo_Typescript/aula_31julho/basics/tipoDado.ts
@@ -1,20 +1,67 @@
 const nomes: string[] = ["Bob"];
 nomes.push("Patrick");
-nomes.push("Antony");
-nomes.pop()
+nomes.push("Antony", "Jack");
+nomes.pop()   // remove o último elemento
+nomes.shift() // remove o primeiro elemento 
+nomes.unshift("Harry", "Jim") // insere no inicio do array
 // imprimir dados
 for (var i = 0; i < nomes.length; i++) {
-    console.log(nomes[i]);
+    console.log(`${i} ${nomes[i]}`);
 }
 
 const dados: [string, number, boolean] = ["Wolnei", 32, false];
 // imprimir dados
 for (var i = 0; i < dados.length; i++) {
-    console.log(`${i} ${dados[i]} ${typeof(dados[i])}`);
+    console.log(`${i} ${dados[i]} ${typeof (dados[i])}`);
 }
 
-const elemento: any[] = [55,"Robert",false];
+const elemento: any[] = [55, "Robert", false];
 // imprimir dados
 for (var i = 0; i < elemento.length; i++) {
-    console.log(`${i} ${elemento[i]} ${typeof(elemento[i])}`);
+    console.log(`${i} ${elemento[i]} ${typeof (elemento[i])}`);
 }
+
+// objeto
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function () {
+        return `Name: ${this.firstName} ${this.lastName}`;
+    }
+};
+console.log(person.fullName());
+
+// Exemplo FOR
+const harryPotter: string[] = ["Harry", "Hermione", "Rony", "Draco", "Luna", "Hermione"];
+var posicaoArray: number[] = [];
+for (i = 0; i < harryPotter.length; i++) {
+    if (harryPotter[i] === "Hermione") {
+        posicaoArray.push(i);
+    }
+}
+console.log(`Hermione[for]: ${posicaoArray}`)
+
+// Exemplo FOR-EACH
+var posicaoArray_2: number[] = [];
+harryPotter.forEach((nome, pos) => {
+    if (nome === "Hermione") {
+        posicaoArray_2.push(pos);
+    }
+})
+console.log(`Hermione[forEach]: ${posicaoArray_2}`)
+
+// outro exemplo forEach
+function exibirNome(nome: string, pos: number){
+    console.log(`- ${nome} esta na posicao ${pos}`);
+}
+harryPotter.forEach(exibirNome);
+
+// Quantidade de Hermione:
+var quantidadeHermione: number = 0;
+harryPotter.forEach((nome) => {
+    if (nome === "Hermione") {
+        quantidadeHermione++;
+    }
+})
+console.log(`Quantidade nomes Hermione: ${quantidadeHermione}.`)
