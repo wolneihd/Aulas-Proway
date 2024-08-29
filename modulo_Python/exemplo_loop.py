4b96d4871ad52e196c4d1cab80893159ffdfc3ef
wolneihd <wolneihd@gmail.com>
2024-08-28 21:35:51 -0300

    Atualização automática de commit

--- modulo_Python/exemplo_loop.py
+++ modulo_Python/exemplo_loop.py
@@ -7,6 +7,7 @@ def exemplo_while01():
         print(indice)
         indice-=1
 
+
 def exemplo_while02():
     indice = 0
     while indice < 3:
@@ -18,6 +19,69 @@ def exemplo_while02():
         print(f'Nome completo: {nome_completo}')
         indice+=1
 
+
+def exemplo_while_descobrir_media(quantidade_notas):
+    lista_notas = []
+    soma = 0
+    while True:
+        try:
+            nota = int(input('Digite a nota: '))
+            lista_notas.append(nota)
+            if len(lista_notas) == quantidade_notas:
+                for numero in lista_notas:
+                    soma += numero          
+                return soma/quantidade_notas
+        except:
+            return 'erro'
+    
+
+def exemplo_maior_valor(quantidade_notas):
+    valor = int(input('Digite o valor: '))
+    maior_valor = valor
+    lista_valores = [maior_valor]
+    while True:
+        try:
+            valor = int(input('Digite o valor: '))
+            lista_valores.append(valor)
+            if valor > maior_valor:
+                maior_valor = valor
+            if len(lista_valores) == quantidade_notas:
+                return f'O maior valor informado é {maior_valor}'
+        except:
+            return 'erro'
+        
+
+def exemplo_menor_valor(quantidade_valores):
+    valor = int(input('Digite o valor: '))
+    menor_valor = valor
+    lista_valores = [valor]
+    while True:
+        try:
+            valor = int(input('Digite o valor: '))
+            lista_valores.append(valor)
+            if valor < menor_valor:
+                menor_valor = valor
+            if len(lista_valores) == quantidade_valores:
+                return f'O menor valor informado é {menor_valor}'
+        except:
+            return 'erro'
+        
+
+def exemplo_condicao_usuario():
+    lista_produtos = []
+    while True:
+        nome_produto = input("Digite o nome do produto: ")
+        if nome_produto.lower() == 'sair':
+            print(lista_produtos)
+            return 'Usuário saiu!'
+        lista_produtos.append(nome_produto)
+
 # chamando as funções:
+
 # exemplo_while01()
-exemplo_while02()
+# exemplo_while02()
+# resultado = exemplo_while_descobrir_media(5)
+# resultado = exemplo_maior_valor(5)
+# resultado = exemplo_menor_valor(5)
+resultado = exemplo_condicao_usuario()
+print(resultado)
