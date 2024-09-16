b2ccbba79beca93dd82320c1faa12371a5e833de
wolneihd <wolneihd@gmail.com>
2024-09-16 19:45:29 -0300

    update 16set

---
+++ modulo_SQL/exemplo-manipulacao.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE super_dev_financeiro;
+USE super_dev_financeiro;
+
+CREATE TABLE contas(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    saldo DOUBLE NOT NULL
+);
+
+INSERT INTO contas (saldo) VALUES
+(129.90),
+(250.00);
+
+select id as 'ID', format(saldo,2) as 'SALDO' from contas;
+
+-- ALTERANDO OS DADOS DA TABELA:
+ALTER TABLE contas RENAME TO despesas;
+ALTER TABLE despesas RENAME COLUMN saldo TO valor;
+ALTER TABLE despesas ADD COLUMN status INT;
+INSERT INTO despesas (valor) VALUES (110);
+
+-- ATUALIZAR DESPESAS:
+UPDATE despesas SET status = 0 WHERE id=1;
+UPDATE despesas SET status = 1 WHERE id=2;
+UPDATE despesas SET status = 1 WHERE id=3;
+
+-- COLUNA despesas será 0 se não informado outro valor:
+ALTER TABLE despesas ALTER COLUMN STATUS SET DEFAULT 0;
+INSERT INTO despesas (valor) VALUES (950.10);
+
+ALTER TABLE despesas ADD COLUMN nome VARCHAR(50);
+ALTER TABLE despesas MODIFY nome VARCHAR(100);
+
+UPDATE despesas SET nome = 'Internet Unifique' WHERE id=1;
+UPDATE despesas SET nome = 'Gasolina' WHERE id=2;
+UPDATE despesas SET nome = 'Restaurante' WHERE id=3;
+UPDATE despesas SET nome = 'Aluguel' WHERE id=4;
+
+ALTER TABLE despesas ADD COLUMN forma_pagamento VARCHAR(20);
+UPDATE despesas SET forma_pagamento = 'pix' WHERE id=1;
+UPDATE despesas SET forma_pagamento = 'cartão de crédito' WHERE id=2;
+UPDATE despesas SET forma_pagamento = 'pix' WHERE id=3;
+UPDATE despesas SET forma_pagamento = 'NULL' WHERE id=4;    
+
+SELECT * FROM despesas WHERE forma_pagamento IS NULL;
