a40c94ec9e61531f892a4f0a061910341673b872
wolneihd <wolneihd@gmail.com>
2024-10-14 22:01:56 -0300

    aula 14out

--- modulo_Django_Angular/steamfake/models.py
+++ modulo_Django_Angular/steamfake/models.py
@@ -3,3 +3,21 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
     nome = models.CharField(max_length=23, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+# class Aluno(models.Model):
+#     nome = models.CharField(max_length=10)
+#     idade = models.IntegerField(default=0)
+#     nota01 = models.DecimalField(default=0, decimal_places=2, max_digits=4)
+#     nota02 = models.DecimalField(default=0, decimal_places=2, max_digits=4)
+#     nota03 = models.DecimalField(default=0, decimal_places=2, max_digits=4)
+
+# class Curso(models.Model):
+#     nome = models.CharField(max_length=50)
+#     duracao = models .IntegerField(default=0)
+
+# class Turma(models.Model):
+#     data_inicio = models.DateTimeField()
+#     curso = models.ForeignKey(Curso, on_delete=models.CASCADE, blank=True, null=True)
--- modulo_Django_Angular/steamfake/views.py
+++ modulo_Django_Angular/steamfake/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+
+from steamfake.models import Categoria
 
 # Create your views here.
+# http://127.0.0.1:8000/steamfake/categoria
+def categoria_index(request: HttpRequest) -> HttpResponse:
+    categorias = Categoria.objects.all()
+    dados = {
+        "categorias": categorias
+    }
+    return render(request, "categorias/index.html", context=dados)
+
+def categoria_cadastro(request: HttpRequest) -> HttpResponse:
+    return render(request, "categorias/cadastro.html")
+
+def categoria_cadastrar(request: HttpRequest) -> HttpResponse:
+    nome = request.POST.get("nome")
+    categoria = Categoria(nome=nome)
+    categoria.save()
+    return redirect("categorias")
+
+def categoria_apagar(request: HttpRequest, id:int) -> HttpResponse:
+    categoria = Categoria.objects.get(pk=id)
+    categoria.delete()
+    return redirect("categorias")
