e58f84b4db6d9e7307f5caddec60251b1cc3261d
Wolnei Hellmann Dircksen <wolneihd@gmail.com>
2024-11-22 10:39:13 -0300

    rpa refactor

--- modulo_RPA/rpa_challenge.py
+++ modulo_RPA/rpa_challenge.py
@@ -7,10 +7,10 @@ import json
 def contador_tempo(segundos: int):
     for i in range(1, segundos+1):
         time.sleep(1)
-        print(f'Tempo de espera: {segundos} | contador: {i}')
+        print(f'Tempo de espera para abrir e carregar a pagina: {segundos} | contador: {i}')
 
 # abrindo a página
-print('Abrindo página RPA CHALLENGE:')
+print('Abrindo página RPA CHALLENGE')
 webbrowser.open('https://rpachallenge.com/')
 contador_tempo(8)
 
@@ -30,126 +30,53 @@ def dados_usuario():
     lista_usuarios = json.loads(dados_json)
     return lista_usuarios    
 
+def buscar_campo(nome_campo:str, informacao_campo:str, just_click:bool):
+    try:
+        time.sleep(1)
+        campo = f'{nome_campo}.png'
+        buscar_campo = pyautogui.locateOnScreen(campo, confidence=0.8)
+
+        if buscar_campo and just_click == False:
+            pyautogui.click(buscar_campo)
+            pyautogui.typewrite(informacao_campo)            
+        elif buscar_campo and just_click == True:
+            pyautogui.click(buscar_campo)
+        else:
+            print(f'Campo {nome_campo} não localizado | informação não adicionada: {informacao_campo}')
+            return
+    except Exception as error:
+        print(f"Erro ao inserir dados {nome_campo} | informação não adicionada: {informacao_campo}")
+        print('Erro: ', error)
+
 def preencher_formulario(usuario: Usuario):  
+ 
+    print(f'Preenchendo os campos do(a) usuário(a): {usuario.first_name}')
 
-    confidence = 0.8    
-    print(f'Iniciando a preeencher os campos do usuário {usuario.first_name} ... ')
-
-    try:
-        time.sleep(1)
-        campo_address = pyautogui.locateOnScreen('address.png', confidence=confidence)
-        if campo_address:
-            pyautogui.click(campo_address)
-            pyautogui.typewrite(usuario.address)
-        else:
-            print('Campo login não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao inserir dados campo_address - ", error)
-
-    try:
-        time.sleep(1)
-        email = pyautogui.locateOnScreen('email.png', confidence=confidence)
-        if email:
-            pyautogui.click(email)
-            pyautogui.typewrite(usuario.email)
-        else:
-            print('Campo login não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao inserir dados email - ", error)
-
-    try:
-        time.sleep(1)
-        first_name = pyautogui.locateOnScreen('first_name.png', confidence=confidence)
-        if first_name:
-            pyautogui.click(first_name)
-            pyautogui.typewrite(usuario.first_name)
-        else:
-            print('Campo login não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao inserir dados first_name - ", error)
-
-    try:
-        time.sleep(1)
-        last_name = pyautogui.locateOnScreen('last_name.png', confidence=confidence)
-        if last_name:
-            pyautogui.click(last_name)
-            pyautogui.typewrite(usuario.last_name)
-        else:
-            print('Campo login não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao inserir dados last_name - ", error)
-
-    try:
-        time.sleep(1)
-        role = pyautogui.locateOnScreen('role.png', confidence=confidence)
-        if role:
-            pyautogui.click(role)
-            pyautogui.typewrite(usuario.role)
-        else:
-            print('Campo login não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao inserir dados role - ", error)
-
-    try:
-        time.sleep(1)
-        phone = pyautogui.locateOnScreen('phone.png', confidence=confidence)
-        if phone:
-            pyautogui.click(phone)
-            pyautogui.typewrite(str(usuario.phone))
-        else:
-            print('Campo login não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao inserir dados phone - ", error)
-
-    try:
-        time.sleep(1)
-        company = pyautogui.locateOnScreen('company.png', confidence=confidence)
-        if company:
-            pyautogui.click(company)
-            pyautogui.typewrite(usuario.company)
-        else:
-            print('Campo login não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao inserir dados company - ", error)
-
-    # envia dados
-    try:
-        time.sleep(1)
-        submit = pyautogui.locateOnScreen('submit.png', confidence=confidence)
-        if company:
-            pyautogui.click(submit)
-        else:
-            print('Campo SUBMIT não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao ENVIAR dados", error)
-
-def start_game():
-    try:
-        start = pyautogui.locateOnScreen('start.png', confidence=0.8)
-        if start:
-            pyautogui.click(start)
-            print('clicado START')
-        else:
-            print('Campo START não encontrado!')
-            return
-    except Exception as error:
-        print(f"Erro ao START", error)
+    buscar_campo('address', usuario.address, False)
+    buscar_campo('email', usuario.email, False)
+    buscar_campo('first_name', usuario.first_name, False)
+    buscar_campo('last_name', usuario.last_name, False)
+    buscar_campo('role', usuario.role, False)
+    buscar_campo('phone', str(usuario.phone), False)
+    buscar_campo('company', usuario.company, False)
+    buscar_campo('submit', None, True)
 
 if __name__ == "__main__":
     # START o jogo
-    start_game()
+    buscar_campo('start', None, True)
 
     # iniciar preencher os dados
     lista_usuarios = dados_usuario()
     for usuario in lista_usuarios:
-        user = Usuario(usuario['First Name'], usuario['Last Name'], usuario['Address'], usuario['Email'], usuario['Company Name'], usuario['Phone Number'], usuario['Role in Company'])
+        user = Usuario(
+            usuario['First Name'], 
+            usuario['Last Name'], 
+            usuario['Address'], 
+            usuario['Email'], 
+            usuario['Company Name'], 
+            usuario['Phone Number'], 
+            usuario['Role in Company'])
         preencher_formulario(user)
+
+    print('Preenchimento finalizado!')
     
